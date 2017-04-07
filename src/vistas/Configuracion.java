33f85788843ffc08cc02e7d5f679be086e485e31
Mervin Díaz <mervindiazlugo@gmail.com>
2017-04-06 23:09:13 -0300

    Inicio de Pantallas de inicio de proyectos

--- src/vistas/Configuracion.java
+++ src/vistas/Configuracion.java
@@ -8,11 +8,18 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import org.apache.commons.io.FileUtils;
+
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Locale;
@@ -23,12 +30,15 @@ import java.awt.Font;
 public class Configuracion extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtID;
+	private JTextField txtNombreProject;
 	private JTextField txtPath;
-	String vID, PathReportes, PathProyecto;
+	String vID, PathReportes, PathProyecto,DataProviderPath, resp;
 	int respuesta;
 	private JTextField txtProcessCenter;
 	private JTextField txtPathreportes;
+	PreparedStatement PrepararSentencias;
+	Connection conexionBD;
+	private JTextField txtDataProvider;
 
 	/**
 	 * Launch the application.
@@ -61,14 +71,14 @@ public class Configuracion extends JFrame {
 		btnBuscar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				  //Consultar en BD
-		        if (txtID.getText().equals("")){
+		        if (txtNombreProject.getText().equals("")){
 		            JOptionPane.showMessageDialog(null,"Introduzca el ID del Proyecto","",JOptionPane.PLAIN_MESSAGE);
-		            txtID.requestFocus();
+		            txtNombreProject.requestFocus();
 		        }else
 
 		            try {
 		                Conectar conexionBD= new Conectar();
-		                vID= String.valueOf(txtID.getText());
+		                vID= String.valueOf(txtNombreProject.getText());
 		                System.out.println(vID);
 		                String Cons="select * from testconfig where id="+(vID);
 		                ResultSet consulta=conexionBD.consulta(Cons);
@@ -80,7 +90,7 @@ public class Configuracion extends JFrame {
 		                if(respuesta==0){
 		              
 		                }else
-		                	txtID.requestFocus();
+		                	txtNombreProject.requestFocus();
 		                
 		            }     catch (SQLException error)
 		              {
@@ -98,23 +108,23 @@ public class Configuracion extends JFrame {
 		        
 			}
 		});
-		btnBuscar.setBounds(558, 13, 89, 23);
+		btnBuscar.setBounds(296, 290, 89, 23);
 		contentPane.add(btnBuscar);
 		
-		txtID = new JTextField();
-		txtID.setBounds(226, 44, 245, 22);
-		contentPane.add(txtID);
-		txtID.setColumns(10);
+		txtNombreProject = new JTextField();
+		txtNombreProject.setBounds(229, 70, 245, 22);
+		contentPane.add(txtNombreProject);
+		txtNombreProject.setColumns(10);
 		
 		txtPath = new JTextField();
 		txtPath.setEnabled(false);
-		txtPath.setBounds(226, 83, 245, 23);
+		txtPath.setBounds(229, 109, 245, 23);
 		contentPane.add(txtPath);
 		txtPath.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Nuevo Test Project");
 		lblNewLabel.setFont(new Font("Arial Narrow", Font.BOLD, 16));
-		lblNewLabel.setBounds(275, 11, 126, 22);
+		lblNewLabel.setBounds(278, 37, 126, 22);
 		contentPane.add(lblNewLabel);
 		
 		JButton btnSetPathProyecto = new JButton("Set Path");
@@ -128,55 +138,89 @@ public class Configuracion extends JFrame {
 			    chooser.setAcceptAllFileFilterUsed(false);
 
 			    if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-			      txtPathreportes.setText(String.valueOf(chooser.getSelectedFile()));
+			      txtPath.setText(String.valueOf(chooser.getSelectedFile()));
 			      PathProyecto=String.valueOf(chooser.getSelectedFile());
-			      System.out.println(PathProyecto);
 			      PathProyecto= PathProyecto.replace("\\", "\\\\");
-			      System.out.println(PathProyecto);
+			      ComprobarDirectorio();
 	
 			    } else {
 			    	JOptionPane.showMessageDialog(null,"Debes seleccionar un directorio","",JOptionPane.PLAIN_MESSAGE);
 			    }
+			    
+			    
 			  }
 				
-			
+
 		});
-		btnSetPathProyecto.setBounds(478, 83, 89, 23);
+		btnSetPathProyecto.setBounds(481, 109, 89, 23);
 		contentPane.add(btnSetPathProyecto);
 		
 		JLabel lblNombreDeProyecto = new JLabel("Nombre de Proyecto");
 		lblNombreDeProyecto.setFont(new Font("Arial Narrow", Font.BOLD, 14));
-		lblNombreDeProyecto.setBounds(109, 42, 113, 22);
+		lblNombreDeProyecto.setBounds(112, 68, 113, 22);
 		contentPane.add(lblNombreDeProyecto);
 		
 		JLabel lblPathDelProyecto = new JLabel("URL Process Center");
 		lblPathDelProyecto.setFont(new Font("Arial Narrow", Font.BOLD, 14));
-		lblPathDelProyecto.setBounds(109, 149, 113, 22);
+		lblPathDelProyecto.setBounds(106, 209, 113, 22);
 		contentPane.add(lblPathDelProyecto);
 		
 		JLabel label = new JLabel("Path del Proyecto");
 		label.setFont(new Font("Arial Narrow", Font.BOLD, 14));
-		label.setBounds(119, 81, 97, 22);
+		label.setBounds(122, 107, 97, 22);
 		contentPane.add(label);
 		
 		txtProcessCenter = new JTextField();
 		txtProcessCenter.setColumns(10);
-		txtProcessCenter.setBounds(226, 151, 341, 23);
+		txtProcessCenter.setBounds(229, 211, 341, 23);
 		contentPane.add(txtProcessCenter);
 		
 		JLabel lblNombreDelReportes = new JLabel("Path de Reportes");
 		lblNombreDelReportes.setFont(new Font("Arial Narrow", Font.BOLD, 14));
-		lblNombreDelReportes.setBounds(125, 113, 91, 22);
+		lblNombreDelReportes.setBounds(128, 139, 91, 22);
 		contentPane.add(lblNombreDelReportes);
 		
 		txtPathreportes = new JTextField();
 		txtPathreportes.setEnabled(false);
 		txtPathreportes.setColumns(10);
-		txtPathreportes.setBounds(226, 117, 245, 23);
+		txtPathreportes.setBounds(229, 143, 245, 23);
 		contentPane.add(txtPathreportes);
 		
 		JButton btnCrearProyecto = new JButton("Crear Proyecto");
-		btnCrearProyecto.setBounds(470, 241, 105, 23);
+		btnCrearProyecto.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+		if (txtNombreProject.getText().equals("") | txtPath.getText().equals("") | txtPathreportes.getText().equals("")| txtProcessCenter.getText().equals(""))
+		{
+		    JOptionPane.showMessageDialog(null,"Debe completar todos los campos","",JOptionPane.PLAIN_MESSAGE);
+		    txtNombreProject.requestFocus();
+		}else{	
+				
+			    try {
+			        Conectar conn = new Conectar();
+			        String NuevoSt="insert into testconfig values(?,?,?,?,?,?)";
+			        conn.PrepararSentencias=conn.conexionBD.prepareStatement(NuevoSt);
+			        conn.PrepararSentencias.setString(1,null);
+			        conn.PrepararSentencias.setString(2,txtNombreProject.getText());
+			        conn.PrepararSentencias.setString(3,PathProyecto);
+			        conn.PrepararSentencias.setString(4,txtProcessCenter.getText());
+			        conn.PrepararSentencias.setString(5,PathReportes);
+			        conn.PrepararSentencias.setString(6,DataProviderPath);
+			        conn.PrepararSentencias.executeUpdate();
+			        JOptionPane.showMessageDialog(null, "Nuevo Proyecto Creado");
+			        
+			        CopiarAssets();
+			        
+			        cancelar();
+			        
+			    } catch (Exception e) {
+			        System.out.println(e.getCause());
+			    }
+			}   
+		
+
+			}
+		});
+		btnCrearProyecto.setBounds(436, 245, 134, 23);
 		contentPane.add(btnCrearProyecto);
 		
 		JButton btnPathReportes = new JButton("Set Path");
@@ -192,9 +236,7 @@ public class Configuracion extends JFrame {
 			    if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
 			      txtPathreportes.setText(String.valueOf(chooser.getSelectedFile()));
 			      PathReportes=String.valueOf(chooser.getSelectedFile());
-			      System.out.println(PathReportes);
 			      PathReportes= PathReportes.replace("\\", "\\\\");
-			      System.out.println(PathReportes);
 	
 			    } else {
 			    	JOptionPane.showMessageDialog(null,"Debes seleccionar un directorio","",JOptionPane.PLAIN_MESSAGE);
@@ -203,8 +245,88 @@ public class Configuracion extends JFrame {
 				
 			
 		});
-		btnPathReportes.setBounds(478, 117, 89, 23);
+		btnPathReportes.setBounds(481, 143, 89, 23);
 		contentPane.add(btnPathReportes);
-	}
+		
+		JLabel lblPathDeDataprovider = new JLabel("Path de DataProvider");
+		lblPathDeDataprovider.setFont(new Font("Arial Narrow", Font.BOLD, 14));
+		lblPathDeDataprovider.setBounds(106, 175, 119, 22);
+		contentPane.add(lblPathDeDataprovider);
+		
+		txtDataProvider = new JTextField();
+		txtDataProvider.setEnabled(false);
+		txtDataProvider.setColumns(10);
+		txtDataProvider.setBounds(229, 177, 245, 23);
+		contentPane.add(txtDataProvider);
+		
+		JButton btnDataProvider = new JButton("Set Path");
+		btnDataProvider.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				JFileChooser chooser = new JFileChooser();
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("EXCEL FILES", "xlsx", "xls");
+				chooser.setFileFilter(filter);
+			    chooser.setCurrentDirectory(new java.io.File("."));
+			    chooser.setDialogTitle("Seleccionar Path");
+			    chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			    chooser.setAcceptAllFileFilterUsed(false);
 
+			    if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			      txtDataProvider.setText(String.valueOf(chooser.getSelectedFile()));
+			      DataProviderPath=String.valueOf(chooser.getSelectedFile());
+			      DataProviderPath= PathReportes.replace("\\", "\\\\");
+	
+			    } else {
+			    	JOptionPane.showMessageDialog(null,"Debes seleccionar un directorio","",JOptionPane.PLAIN_MESSAGE);
+			    }
+			  }
+			
+		});
+		btnDataProvider.setBounds(481, 177, 89, 23);
+		contentPane.add(btnDataProvider);
+	}
+	
+	public void cancelar()
+	{
+		txtNombreProject.setText("");
+		txtPath.setText("");
+		txtProcessCenter.setText("");
+		txtPathreportes.setText("");
+		txtDataProvider.setText("");
+		
+	}
+	
+
+	    public void ComprobarDirectorio()
+	    {
+
+		File file = new File(PathProyecto);
+
+		if(file.isDirectory()){
+
+			if(file.list().length>0){
+
+				int respuesta = JOptionPane.showOptionDialog(null,"El directorio seleccionado no esta vacio, ¿Desea Continuar?", "Directorio en uso", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
+								if(respuesta == 0 ){
+									
+								}else{
+									txtPath.setText("");
+								}
+			}
+		}
+	    }
+	    
+	    public void CopiarAssets()
+	    {
+			File source = new File("C:\\workspace\\Repo\\AssetsTest");
+			File dest = new File(PathProyecto);
+			try {
+			    FileUtils.copyDirectory(source, dest);
+			    
+			    
+			} catch (IOException e) {
+			    e.printStackTrace();
+			}
+	    	
+	    }
 }
